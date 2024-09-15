6d0c7df7d2b5f96c3d8fed4001ffbbe7b56148fb
yaroslava <sosninaaroslava@gmail.com>
2024-09-15 11:05:46 +0600

    homework done

---
+++ src/main/kotlin/com/yaroslava/kotlincource/lesson4/homework4.kt
@@ -0,0 +1,45 @@
+package com.yaroslava.kotlincource.lesson4
+
+class homework4 {
+    val myInt: Int = 42
+    val myLong: Long = 98765432123456789L
+    val myFloat: Float = 23.45f
+    val myDouble: Double = 0.123456789
+    val myString: String = "Kotlin & Java"
+    val myBoolean: Boolean = false
+    val myChar: Char = 'c'
+   val secInt: Int =  500
+    val secLong: Long = 4294967296L
+    val secFloat: Float = 18.0f
+    val secDouble: Double = -0.001
+    val secString: String = "OpenAI"
+    val thString: String = "true”
+    val secChar: Char = '9'
+    val thInt: Int = 2048
+    val thLong: Long = 10000000000L
+    val thFloat: Float = 5.75f
+    val thDouble: Double = 1.414
+    val fourString: String = "Artificial Intelligence"
+    val secBoolean: Boolean = false
+    val thChar: Char = '@'
+    val fourInt: Int = 1024
+    val fourLong: Long = 1234567890123L
+    val fourFloat: Float = 10.01f
+    val fourDouble: Double = -273.15
+    val fiveString: String = "SpaceX"
+    val thBoolean: Boolean = true
+    val sixString: String = “🤯”
+    val sevenString: String ="‘65535’"
+    val fiveLong: Long = 72057594037927935L
+    val fiveFloat: Float = 2.71828f
+    val fiveDouble: Double = 101.0101
+    val eightString: String = "Quantum Computing"
+    val fourBoolean: Boolean = false
+    val fourChar: Char = 'x'
+    val myShort: Short = 314
+    val sixLong: Long = 123456789123456789L
+    val sixFloat: Float = 6.626f
+    val sixDouble: Double = 0.007
+    val nineString: String ="Android Studio"
+
+}
