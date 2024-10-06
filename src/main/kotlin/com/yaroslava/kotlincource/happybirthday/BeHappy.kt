d8b0c8950ad3016091cb6e09785247b9b10e7c1f
yaroslava <sosninaaroslava@gmail.com>
2024-10-06 20:54:32 +0600

    happy birthday

---
+++ src/main/kotlin/com/yaroslava/kotlincource/happybirthday/BeHappy.kt
@@ -0,0 +1,13 @@
+package com.yaroslava.kotlincource.happybirthday
+
+class BeHappy {
+fun main() {
+    val congrats : List<String> = listOf(
+        "счастья",
+        "здоровья",
+        "терпения",
+        "успеха во всем"
+    )
+    println(congrats)
+}
+}
